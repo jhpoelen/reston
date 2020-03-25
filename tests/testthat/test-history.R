f1ab0eafa80db5e383318e5d9d92a159b05fb7e2
jhpoelen <jhpoelen@xs4all.nl>
2020-03-25 16:47:53 -0700

    add lazy iterator for retrieving provenance versions. fyi @cboettig

--- tests/testthat/test-history.R
+++ tests/testthat/test-history.R
@@ -1,3 +1,5 @@
+library(iterators)
+
 context("retrieve version history")
 
 sha256_uri <- function(x) {
@@ -29,6 +31,24 @@ is_valid_hash <- function(hash) {
   ifelse(length(grep("hash://sha256/[0-9a-z]{64}", hash)) == 0, FALSE, TRUE)
 }
 
+version_history_iter <- function(retrieve, queryHash = first_version_query_hash()) {
+  query_hash <- first_version_query_hash()
+
+  nextEl <- function() {
+    candidate_version <- retrieve(query_hash)
+    if (is_valid_hash(candidate_version)) {
+      query_hash <<- next_version_query_hash(candidate_version)
+      candidate_version
+    } else {
+      stop('StopIteration')
+    }
+  }
+
+  obj <- list(nextElem = nextEl)
+  class(obj) <- c('prov_history_iter','abstractiter','iter')
+  obj
+}
+
 version_history <- function(retrieve) {
   versions <- function(version_history) {
     last_version <- version_history[[length(version_history)]]
@@ -42,6 +62,24 @@ version_history <- function(retrieve) {
   first_version <- retrieve(first_version_query_hash())
   versions(list(first_version))
 }
+
+retrieve_test <- function(hash) {
+  switch(hash,
+         "hash://sha256/2a5de79372318317a382ea9a2cef069780b852b01210ef59e06b640a3539cb5a" = "hash://sha256/c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55",
+         "hash://sha256/7ebb008412baaac3afcc8af68b796bf4ca98f367cfd61a815eee82cdffeab196" = "hash://sha256/b83cf099449dae3f633af618b19d05013953e7a1d7d97bc5ac01afd7bd9abe5d",
+         "hash://sha256/1707cb11cd9f696f1a86fd06742c1e14fad856747be88791f79f6fc7c979d5a6" = "hash://sha256/7efdea9263e57605d2d2d8b79ccd26a55743123d0c974140c72c8c1cfc679b93"
+  )
+};
+
+
+test_that("prov version iter returns all versions", {
+  prov_iter <- version_history_iter(retrieve_test)
+  actual_versions = as.list(prov_iter)
+  expect_equal(actual_versions[[1]], "hash://sha256/c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55")
+  expect_equal(actual_versions[[2]], "hash://sha256/b83cf099449dae3f633af618b19d05013953e7a1d7d97bc5ac01afd7bd9abe5d")
+  expect_equal(actual_versions[[3]], "hash://sha256/7efdea9263e57605d2d2d8b79ccd26a55743123d0c974140c72c8c1cfc679b93")
+  expect_equal(length(actual_versions), 3);
+})
 
 test_that("hash uri is valid", {
   expect_true(is_valid_hash("hash://sha256/a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"))
@@ -61,9 +99,6 @@ test_that("can calculate a sha256 hash", {
 })
 
 test_that("can traverse a preston provenance history", {
-
-
-
   expect_identical(
     first_version_query_hash(),
     "hash://sha256/2a5de79372318317a382ea9a2cef069780b852b01210ef59e06b640a3539cb5a"
@@ -89,27 +124,20 @@ test_that("retrieve a sequence of versions", {
   query_hash_second <- "hash://sha256/7ebb008412baaac3afcc8af68b796bf4ca98f367cfd61a815eee82cdffeab196"
   query_hash_third <- "hash://sha256/1707cb11cd9f696f1a86fd06742c1e14fad856747be88791f79f6fc7c979d5a6"
 
-  retrieve <- function(hash) {
-      switch(hash,
-        "hash://sha256/2a5de79372318317a382ea9a2cef069780b852b01210ef59e06b640a3539cb5a" = "hash://sha256/c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55",
-        "hash://sha256/7ebb008412baaac3afcc8af68b796bf4ca98f367cfd61a815eee82cdffeab196" = "hash://sha256/b83cf099449dae3f633af618b19d05013953e7a1d7d97bc5ac01afd7bd9abe5d",
-        "hash://sha256/1707cb11cd9f696f1a86fd06742c1e14fad856747be88791f79f6fc7c979d5a6" = "hash://sha256/7efdea9263e57605d2d2d8b79ccd26a55743123d0c974140c72c8c1cfc679b93"
-      )
-  };
   query <- function(query_hash, retrieve) {
     retrieve(query_hash)
   }
 
-  expect_identical(query(query_hash_first, retrieve),
+  expect_identical(query(query_hash_first, retrieve_test),
                    "hash://sha256/c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55")
-  expect_identical(query(query_hash_second, retrieve),
+  expect_identical(query(query_hash_second, retrieve_test),
                    "hash://sha256/b83cf099449dae3f633af618b19d05013953e7a1d7d97bc5ac01afd7bd9abe5d")
-  expect_identical(query(query_hash_third, retrieve),
+  expect_identical(query(query_hash_third, retrieve_test),
                    "hash://sha256/7efdea9263e57605d2d2d8b79ccd26a55743123d0c974140c72c8c1cfc679b93")
 
 
 
-  some_versions <- version_history(retrieve)
+  some_versions <- version_history(retrieve_test)
 
   expect_identical(some_versions[[1]], "hash://sha256/c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55")
   expect_identical(some_versions[[2]], "hash://sha256/b83cf099449dae3f633af618b19d05013953e7a1d7d97bc5ac01afd7bd9abe5d")
