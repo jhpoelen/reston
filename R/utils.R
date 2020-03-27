c5fb7caf1bcf97d96c620b5693229e44310f39d0
jhpoelen <jhpoelen@xs4all.nl>
2020-03-27 14:36:04 -0700

    apply length

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,12 @@
 hash_to_2level_path <- function(sha256_hash) {
-  hash <- substr(sha256_hash, 15, 79)
+  hash <- substr(sha256_hash, 15, 78)
   first_two <- substr(sha256_hash, 15, 16)
   second_two <- substr(sha256_hash, 17, 18)
   paste0(first_two, "/", second_two, "/", hash)
 }
 
 hash_to_0level_path <- function(sha256_hash) {
-  substr(sha256_hash, 15, 79)
+  substr(sha256_hash, 15, 78)
 }
 
 is_valid_hash <- function(hash) {
