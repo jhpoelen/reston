f7c3d5908f5f1723619226825f40e0db17b8c4cf
jhpoelen <jhpoelen@xs4all.nl>
2020-03-27 16:28:04 -0700

    add renamed files

---
+++ R/retrieve_content.R
@@ -0,0 +1,23 @@
+#' Opens a stream (i.e., connection) to retrieve content associated with content_hash
+#'
+#' @param content_hash content hash of requested content
+#' @param hash2url function that maps a content hash to a url
+#' @return a stream (i.e., connection) of requested content
+#' @examples
+#' \donttest{
+#'  con <- retrieve_content(query())
+#'  first_line <- readLines(con, 1)
+#'  close(con)
+#'  # first line should start with <https://preston.guoda...
+#' }
+#'
+#' @export
+#'
+retrieve_content <- function(content_hash, hash2url = hash2url_ia_biodiversity_dataset_archive) {
+  if (is_valid_hash(content_hash)) {
+    query_url <- hash2url(content_hash)
+    curl::curl(query_url, "rb")
+  } else {
+    stop(paste(content_hash, "is not a valid content hash"))
+  }
+}
---
+++ tests/testthat/test_retrieve_content.R
@@ -0,0 +1,10 @@
+context("retrieve content")
+
+test_that("content can be retrieved from internet archive", {
+  con <- retrieve_content("hash://sha256/c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55")
+  lines <- readLines(con, 1)
+  expected = paste("<https://preston.guoda.org>", "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>",
+        "<http://www.w3.org/ns/prov#SoftwareAgent>",".")
+  expect_equal(lines[[1]], expected)
+  close(con)
+})
